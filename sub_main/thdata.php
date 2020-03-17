af52e9b6a7397154fdec52cdc9a75523f368d1d2
myresumedevmeow <60541606+myresumedevmeow@users.noreply.github.com>
2020-03-17 18:11:28 +0700

    Add files via upload

---
+++ sub_main/thdata.php
@@ -0,0 +1,36 @@
+
+<div id="project">
+    <section class="content">
+        <div class="block-content">
+            <div class="project-title">
+                <h2>  ระบบคลังข้อมูลขนาดใหญ่  </h2>
+
+            </div>
+            <p class="project-description">
+            ใช้ Vuejs - nuxtjs ในการพัฒนา
+            </p>
+              
+          
+           
+                <img src="sub_main/img_all/thdata1.jpg" alt="">
+                <h2>  </h2>
+                <hr><br>
+           
+          
+          
+
+            </p>
+            <div class="project-media">
+
+               
+                <hr><br>
+                               
+                              
+                
+
+
+            </div>
+
+        </div>
+    </section>
+</div>
