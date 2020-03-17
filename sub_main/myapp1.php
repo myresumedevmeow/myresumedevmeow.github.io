4e61f7ce9d2e61aebad0671ace31d6c41a35772f
myresumedevmeow <60541606+myresumedevmeow@users.noreply.github.com>
2020-03-17 18:00:15 +0700

    Add files via upload

---
+++ sub_main/myapp1.php
@@ -0,0 +1,63 @@
+
+<div id="project">
+    <section class="content">
+        <div class="block-content">
+            <div class="project-title">
+                <h2>  แอพ ประเภท E-book android & ios + SEO สำหรับสายแอพ  (ขอไม่ออกนาม)    </h2>
+
+            </div>
+            <p class="project-description">
+            ใช้ react-native ในการพัฒนา
+            </p>
+                <p class="project-description">
+                - รองรับการโหลดข้อมูลรูปภาพ มหาศาล ไม่มีกระตุก/ค้าง ทดสอบด้วย Samsung J2 
+                </p>
+                <p class="project-description">
+                - Api มีการเข้ารหัสเฉพาะทาง ไม่สามารถดักข้อมูลระหว่างทางได้ และทำการถอดรหัสในฝั่ง User
+                </p>
+                <p class="project-description">
+                - ระบบ Favorite  maker เรื่องที่ชอบไว้อ่านทีหลังได้
+                </p>
+                <p class="project-description">
+                - ระบบแจ้งเตือน การอัพเดทต่างๆ
+                </p>
+                <p class="project-description">
+                - ระบบช่วยจำจดการอ่าน / ตอนที่เคยอ่านไปแล้ว 
+                </p>
+                <p class="project-description">
+                - ระบบ history ต่างๆ
+                </p>
+                <p class="project-description">
+                - การค้นหาตาม ตัวอักษร และ หมวดหมู่ ต่างๆ สามารถตามหาแนวที่เราชื่นชอบได้
+                </p>
+           
+                <p class="project-description">
+                - แคชระบบ Cache ช่วยให้ประหยัดเน็ต และเร็วขึ้นมากๆ
+                </p>
+           
+         
+                <p class="project-description">
+                -การปรับรูปแบบหน้าตาของแอพ ตามความต้องการของ แต่ละบุคคล
+                </p>
+          
+           
+              <h2> ค่อนข้างพัฒนาต่อยอดในฝั่งของสื่อประเภท E-book ได้หลากหลายกลุ่ม</h2>
+           
+          
+          
+
+            </p>
+            <div class="project-media">
+
+               
+                <hr><br>
+                               
+                              
+                
+
+
+            </div>
+
+        </div>
+    </section>
+</div>
---
+++ sub_main/myapp2.php
@@ -0,0 +1,78 @@
+
+<div id="project">
+    <section class="content">
+        <div class="block-content">
+            <div class="project-title">
+                <h2> แอพ ประเภท Video streaming support AndroidTV&Mobile android & ios   </h2>
+
+            </div>
+            <p class="project-description">
+            ใช้ react-native ในการพัฒนา
+            </p>
+            
+ 
+ 
+ 
+
+
+
+                <p class="project-description">
+                - รองรับการโหลดข้อมูลรูปภาพ มหาศาล ไม่มีกระตุก/ค้าง ทดสอบด้วย Samsung J2 
+                </p>
+                <p class="project-description">
+                - Api มีการเข้ารหัสเฉพาะทาง ไม่สามารถดักข้อมูลระหว่างทางได้ และทำการถอดรหัสในฝั่ง User
+                </p>
+                <p class="project-description">
+                - ระบบ Favorite  maker เรื่องที่ชอบไว้อ่านทีหลังได้
+                </p>
+                <p class="project-description">
+                - ระบบแจ้งเตือน การอัพเดทต่างๆ
+                </p>
+                <p class="project-description">
+                - ระบบช่วยจำจดการอ่าน / ตอนที่เคยอ่านไปแล้ว 
+                </p>
+                <p class="project-description">
+                - ระบบ history ต่างๆ
+                </p>
+                <p class="project-description">
+                - การค้นหาตาม ตัวอักษร และ หมวดหมู่ ต่างๆ สามารถตามหาแนวที่เราชื่นชอบได้
+                </p>
+           
+                <p class="project-description">
+                - แคชระบบ Cache ช่วยให้ประหยัดเน็ต และเร็วขึ้นมากๆ
+                </p>
+           
+         
+                <p class="project-description">
+                - รองรับ เซิฟเวอร์ที่เก็บ ไฟล์ วีดีโอที่หลากหลาย ประเภทมากๆ
+                     google drive / photo / ok.ru/ fvs.io เป็นต้น
+
+                </p>
+                <p class="project-description">
+                - security url - ที่แสดงผลไม่ซ้ำกัน และมีการแสดงผลอายุแค่ 1 วินาทีเท่านั้น JWT 
+                </p>
+                 
+              
+
+
+           
+              <h2>  ค่อนข้างพัฒนาต่อยอดในฝั่งของสื่อ Video ได้หลากหลายกลุ่ม</h2>
+           
+          
+          
+
+            </p>
+            <div class="project-media">
+
+               
+                <hr><br>
+                               
+                              
+                
+
+
+            </div>
+
+        </div>
+    </section>
+</div>
